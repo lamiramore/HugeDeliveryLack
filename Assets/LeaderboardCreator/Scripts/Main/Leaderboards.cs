0c357b0ad9a25e42380110932c6ec923004d3866
lamiramore <mira.gollwitzer@htk.academy>
2025-12-12 12:24:49 +0100

    ADDED: Leaderboard

--- Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -2,8 +2,6 @@ namespace Dan.Main
 {
     public static class Leaderboards
     {
-        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
-        public static LeaderboardReference HDL_Leaderboard = new LeaderboardReference("6fc5f4b0d89490a5e8801455fe8d8ccf9df1318cd1d3f3fb0da2f37c14c78253 ");
-        public static LeaderboardReference HugeDeliverLead = new LeaderboardReference("6fc5f4b0d89490a5e8801455fe8d8ccf9df1318cd1d3f3fb0da2f37c14c78253 ");
+        public static LeaderboardReference HugeDeliverLead = new LeaderboardReference("6fc5f4b0d89490a5e8801455fe8d8ccf9df1318cd1d3f3fb0da2f37c14c78253");
     }
 }
